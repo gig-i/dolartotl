8bdf0c859b466c3873aad700ec29f8a9eb711825
GizemKılıçaslan <gizem.k.aslan@gmail.com>
2021-06-23 19:06:54 +0300

    app

---
+++ app/src/main/java/com/example/dolartotl/model/quotes.java
@@ -0,0 +1,16 @@
+package com.example.dolartotl.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class quotes {
+
+    @SerializedName("USDTRY")
+    private float USDTRY;
+
+    public float getUSDTRY() {
+        return USDTRY;
+    }
+
+
+
+}
